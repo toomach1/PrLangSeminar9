a6fcece371b3233781edaee2437e8987db767169
Artem <temazasport@mail.ru>
2022-12-01 13:14:54 +0800

    HW_task_2

---
+++ HW_task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+int num1 = SetNumber("M");
+
+int num2 = SetNumber("N");
+
+int summ = 0;
+
+if(num1 < num2)
+{
+    while (num1 <= num2)
+    {
+    summ = num1+summ;
+    num1++;
+    }
+}
+else
+{
+   while (num2 <= num1)
+    {
+    summ = num2+summ;
+    num2++;
+    } 
+}
+
+System.Console.WriteLine(summ);
+
+
+
+int SetNumber(string str) //метод "введите Х"
+{
+
+    System.Console.WriteLine($"Введите {str}");
+    int num = int.Parse(Console.ReadLine());
+
+    return num;
+}
---
+++ task1/Program.cs
@@ -0,0 +1,88 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+System.Console.WriteLine("начнем считать");
+
+string str = Console.ReadLine();
+
+int i = 0;
+char operat = '+';
+int act = 0;
+int num1 = 0;
+int num2 = 0;
+int index = 0;
+
+while (i < str.Length)
+{
+    if (act == 0)
+    {
+        if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
+        {
+            operat = str[i];
+            string numString1 = "";
+            for (int j = 0; j < i; j++)
+            {
+                numString1 += str[j];
+            }
+            num1 = Convert.ToInt32(numString1);
+            i++;
+            act++;
+            index = i;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    else
+    {
+        if (str[i] != '=')
+        {
+            i++;
+
+        }
+        else
+        {
+            string numString2 = "";
+            for (int j = index; j < i; j++)
+            {
+                numString2 += str[j];
+            }
+            num2 = Convert.ToInt32(numString2);
+            SolveTheEq(num1, num2, operat);
+            return;
+        }
+    }
+}
+
+void SolveTheEq(int number1, int number2, char operation)
+{
+    int dec;
+    if (operation == '+')
+    {
+        dec = number1 + number2;
+        System.Console.WriteLine($"ответ {dec}");
+        return;
+    }
+
+    if (operation == '-')
+    {
+        dec = number1 - number2;
+        System.Console.WriteLine($"ответ {dec}");
+        return;
+    }
+
+    if (operation == '/')
+    {
+        dec = number1 / number2;
+        System.Console.WriteLine($"ответ {dec}");
+        return;
+    }
+
+    if (operation == '*')
+    {
+        dec = number1 * number2;
+        System.Console.WriteLine($"ответ {dec}");
+        return;
+    }
+
+}
